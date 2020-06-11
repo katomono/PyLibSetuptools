d151f719dffe1a2056ab1ff253ae2030049dcdae
kibou227mirai <kibou11037mirai@gmail.com>
2020-06-11 09:19:56 +0900

    サンプル

---
+++ src/library/utils/sample1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+def helloworld():
+    """
+    helloworld
+    """
+    print("HelloWorld")
+
+
+class SampleClass:
+    """
+    SampleClass
+    """
+
+    def __init__(self, a: int, b: str):
+        """
+        Description.__init___
+        """
+        self.a = a
+        self.b = b
+
